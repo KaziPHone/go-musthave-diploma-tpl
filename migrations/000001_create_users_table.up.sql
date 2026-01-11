1451b706f258007a4442262097e9501960a81011
kaziphone <kazimirskiy87@gmail.com>
2026-01-11 19:20:09 +0300

    fix migration

--- migrations/000001_create_users_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users(
-    id integer GENERATED ALWAYS AS IDENTITY NOT NULL,
-    create_time timestamp without time zone DEFAULT '2026-01-10 19:28:13.459931'::timestamp without time zone,
-    login varchar(255),
-    password varchar(255),
-    PRIMARY KEY(id)
-);
