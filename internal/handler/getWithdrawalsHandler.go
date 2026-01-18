fb8e918f618e9da1875f38038502614088ef46a4
kaziphone <kazimirskiy87@gmail.com>
2026-01-18 04:23:22 +0300

    fix WithdrawHandler

--- internal/handler/getWithdrawalsHandler.go
+++ internal/handler/getWithdrawalsHandler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/KaziPHone/go-musthave-diploma-tpl/pkg/user"
@@ -16,6 +17,7 @@ func (h *Handler) GetWithdrawalsHandler(w http.ResponseWriter, r *http.Request) 
 	rows, err := h.Storage.DBStorage.GetRows(r.Context(), query, userID)
 
 	if err != nil || rows == nil {
+		fmt.Println(err, "ssss")
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
@@ -26,6 +28,7 @@ func (h *Handler) GetWithdrawalsHandler(w http.ResponseWriter, r *http.Request) 
 		var operation user.Operation
 		err := rows.Scan(&operation.ID, &operation.UserID, &operation.Type, &operation.Amount, &operation.Order, &operation.ProcessedAt)
 		if err != nil {
+			fmt.Println(err, "rows")
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
